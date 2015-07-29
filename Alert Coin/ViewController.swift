56d9ec1c231301e127db507c944d8d218fcb8785
Luiz Ilha <luizilha@Luizs-MacBook-Pro.local>
2015-07-29 09:56:51 -0300

    Initial Commit

---
+++ Alert Coin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alert Coin
+//
+//  Created by Luiz Ilha on 7/29/15.
+//  Copyright (c) 2015 Ilha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
