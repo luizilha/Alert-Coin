a5de5d5514a306f71c208c9252391b5d32acf215
Luiz Ilha <luizilha@Luizs-MacBook-Pro.local>
2015-08-21 23:02:47 -0300

    First Commit.. j

---
+++ Alert Coin/CoinValues.swift
@@ -0,0 +1,13 @@
+//
+//  CoinValues.swift
+//  Alert Coin
+//
+//  Created by Luiz Ilha on 8/21/15.
+//  Copyright (c) 2015 Ilha. All rights reserved.
+//
+
+import Foundation
+
+class CoinValues: NSObject {
+    
+}
